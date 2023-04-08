fd5f34f76d1356e2398d296579113dff28e2a9c3
ikishichi <shino.aniesu1126@gmail.com>
2023-04-09 00:33:49 +0900

    first commit

---
+++ src/deck.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""デッキ情報管理クラス"""
+import requests
+from bs4 import BeautifulSoup
+
+class Deck:
+    def __init__(self, html):
+        """Initialize this class
+
+        Args:
+            html (_type_): _description_
+        """
