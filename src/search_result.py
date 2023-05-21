b6a5db0de8c9cce368fe2316bfb8f6c5537d0747
shhig <shigihara5@gmail.com>
2023-05-21 17:00:25 +0900

    検索結果のシンボル仕様をコメント追記

--- src/search_result.py
+++ src/search_result.py
@@ -1,14 +1,30 @@
+import pandas as pd
 class SearchResult:
-    def __init__(self, MonsterList, origin, destination):
+    def __init__(self, monster_list: pd.DataFrame, origin, destination=''):
         """Initialize this class
 
         Args:
-            MonsterList (string): Deck URL
+            monster_list: (pandas) name, attribute, type, level, attack, defence
             origin: for Search
             destination: for Search
         """
-        self.search_result = []
-        # MonsterListの列はname, attribute, type, level, attack, defence
+        self.search_result = [[]]
+
+    def __name_search(self, monster_list: pd.DataFrame, target_name):
+        monster_list
 
     def get(self):
+        """Get search result
+
+        Args:
+            monster_list: (pandas) name, attribute, type, level, attack, defence
+            origin: for Search
+            destination: for Search
+        Return:
+            2D list about destinations and transit points
+            [search_dest_A, [transit_1, transit_2, transit_3, ...]], 
+            [search_dest_B, [transit_1, transit_2, transit_3, ...]],
+            [search_dest_C, [transit_1, transit_2, transit_3, ...]],
+            ...
+        """
         return self.search_result
