be8f516190f446d2b37746ef0ff631e815ebb45c
shhig <shigihara5@gmail.com>
2023-06-18 20:40:51 +0900

    httpとhttps両方に対応するようにテストコードを修正。

--- test/deckinfo_test.py
+++ test/deckinfo_test.py
@@ -2,7 +2,16 @@ import pytest
 import requests
 from src import deckinfo
 
-def test_success_true(mocker):
+def test_success_true_http(mocker):
+    test_response = requests.Response
+    status_code_ydb = 200
+    test_response.status_code = status_code_ydb
+    mocker.patch("requests.get", return_value = test_response)
+    #mocker.patch("src.deckinfo.DeckInfo.success", return_value = True)
+    di = deckinfo.DeckInfo("http://www.db.yugioh-card.com/yugiohdb/member_deck.action?cgid=testtesttest")
+    assert True == di.success()
+
+def test_success_true_https(mocker):
     test_response = requests.Response
     status_code_ydb = 200
     test_response.status_code = status_code_ydb
