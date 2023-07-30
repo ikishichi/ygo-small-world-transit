5a372da1f571647a87ba61abd7c2788a0f565813
ikishichi <shino.aniesu1126@gmail.com>
2023-07-30 17:47:21 +0900

    deckクラスからdfを受け取るところまで実装

--- src/ui.py
+++ src/ui.py
@@ -3,6 +3,8 @@
 """
 import streamlit as st
 import pandas as pd
+from deckinfo import DeckInfo
+from deck import Deck
 
 st.title("遊戯王スモール・ワールド乗り換え検索")
 st.caption("遊戯王DBの公開デッキを読み込むことで、<<スモール・ワールド>>のサーチ経路を検索できます。")
@@ -13,57 +15,73 @@ monsters_df = pd.DataFrame(columns=columns)
 with st.form(key="deck_url"):
 
     # URL入力欄の入力値
-    url = st.text_input("デッキURL")
+    url = st.text_input("遊戯王DBの公開デッキのURLを入力してください。")
     
     # デッキ取得ボタンの押下状態（boolean）
     submit_btn = st.form_submit_button("デッキ取得")
 
-    if submit_btn:
-        # TODO: 送信ボタンが押されたら、DeckInfoクラスにURLを渡す。
-        # TODO: 取得成功・失敗を表示。（失敗：処理終了 | 成功：処理継続）
-        # TODO: Deckクラスにhtmlを渡す（try-exceptして例外が出たら処理終了）
-        st.text(f"Deck URL: {url}")
-        monsters_df = pd.DataFrame([{'name': 'エフェクト・ヴェーラー', 'attribute': '光属性', 'type': '【魔法使い族／チューナー／効果】', 'level': 'レベル1', 'attack': '攻撃力/0', 'defence': '守備力/0'}, {'name': 'ドットスケーパー', 'attribute': '地属性', 'type': '【サイバース族／効果】', 'level': 'レベル1', 'attack': '攻撃力/0', 'defence': '守備力/2100'}, {'name': 'フォーマッド・スキッパー', 'attribute': '光属性', 'type': '【サイバース族／効果】', 'level': 'レベル1', 'attack': '攻撃力/0', 'defence': '守備力/0'}, {'name': 'マイクロ・コーダー', 'attribute': '闇属性', 'type': '【サイバース族／効果】', 'level': 'レベル1', 'attack': '攻撃力/300', 'defence': '守備力/0'}, {'name': '増殖するG', 'attribute': '地属性', 'type': '【昆虫族／効果】', 'level': 'レベル2', 'attack': '攻撃力/500', 'defence': '守備力/200'}, {'name': '夢幻崩界イヴリース', 'attribute': '闇属性', 'type': '【サイバース族／効果】', 'level': 'レベル2', 'attack': '攻撃力/0', 'defence': '守備力/0'}, {'name': '灰流うらら', 'attribute': '炎属性', 'type': '【アンデット族／チューナー／効果】', 'level': 'レベル3', 'attack': '攻撃力/0', 'defence': '守備力/1800'}, {'name': 'コード・ジェネレーター', 'attribute': '地属性', 'type': '【サイバース族／効果】', 'level': 'レベル3', 'attack': '攻撃力/1300', 'defence': '守備力/500'}, {'name': '斬機シグマ', 'attribute': '光属性', 'type': '【サイバース族／チューナー／効果】', 'level': 'レベル4', 'attack': '攻撃力/1000', 'defence': '守備力/1500'}, {'name': '斬機アディオン', 'attribute': '炎属性', 'type': '【サイバース族／効果】', 'level': 'レベル4', 'attack': '攻撃力/1000', 'defence': '守備力/1000'}, {'name': '斬機サブトラ', 'attribute': '炎属性', 'type': '【サイバース族／効果】', 'level': 'レベル4', 'attack': '攻撃力/1000', 'defence': '守備力/1000'}, {'name': '斬機ダイア', 'attribute': '光属性', 'type': '【サイバース族／チューナー／効果】', 'level': 'レベル4', 'attack': '攻撃力/1000', 'defence': '守備力/1500'}, {'name': '斬機サーキュラー', 'attribute': '光属性', 'type': '【サイバース族／効果】', 'level': 'レベル4', 'attack': '攻撃力/1500', 'defence': '守備力/1500'}, {'name': 'パラレルエクシード', 'attribute': '風属性', 'type': '【サイバース族／効果】', 'level': 'レベル8', 'attack': '攻撃力/2000', 'defence': '守備力/2000'}, {'name': 'ガッチリ＠イグニスター', 'attribute': '地属性', 'type': '【サイバース族／効果】', 'level': 'レベル8', 'attack': '攻撃力/0', 'defence': '守備力/3000'}])
-    
-    # サーチ元指定（プルダウン）
-    transit_start = st.selectbox("サーチ元", monsters_df)
-    
-    # TODO: サーチ元に指定されたモンスターでSearchResultクラスに検索要求する。
-    # TODO: 検索結果をUI表示。
-    # TODO: 検索結果のモンスター名一覧を取得し、サーチ先プルダウンに表示する。
-    
-    # サーチ先指定（プルダウン）
-    # 検索結果の中から候補を選ぶ「絞り込み検索」
-    transit_goal = st.selectbox("サーチ先", monsters_df)
-    
-    col1, col2, col3 = st.columns(3)
-    
-    with col1:
-        st.header("サーチ元")
-        st.write("マイクロ・コーダー")
-        st.write("マイクロ・コーダー")
-        st.write("マイクロ・コーダー")
-        st.markdown("---")
-        st.write("マイクロ・コーダー")
-        st.write("マイクロ・コーダー")
-        st.write("マイクロ・コーダー")
-        
-    with col2:
-        st.header("経由")
-        st.write("斬機シグマ")
-        st.write("斬機サブトラ")
-        st.write("斬機アディオン")
-        st.markdown("---")
-        st.write("斬機シグマ")
-        st.write("斬機サブトラ")
-        st.write("斬機アディオン")
-    
-    with col3:
-        st.header("サーチ先")
-        st.write("ガッチリ＠イグニスター")
-        st.write("ガッチリ＠イグニスター")
-        st.write("ガッチリ＠イグニスター")
-        st.markdown("---")
-        st.write("夢幻崩界イヴリース")
-        st.write("夢幻崩界イヴリース")
-        st.write("夢幻崩界イヴリース")
+    try:
+        if submit_btn:
+            # TODO: Deckクラスにhtmlを渡す（try-exceptして例外が出たら処理終了）
+
+            di = DeckInfo(url)
+            if di.is_success() is False:
+                st.warning("無効なURLです。遊戯王DBの公開デッキレシピのURLを入力してください。")
+                st.stop()
+
+            # st.info("正常なデッキURLです。")
+
+            deck = Deck(di.get())
+            monsters_df = deck.get_monsters_df()
+
+        # サーチ元指定（プルダウン）
+        transit_start = st.selectbox("サーチ元となるモンスターを選択してください。", monsters_df)
+
+        # TODO: サーチ元に指定されたモンスターでSearchResultクラスに検索要求する。
+        # TODO: 検索結果をUI表示。
+        # TODO: 検索結果のモンスター名一覧を取得し、サーチ先プルダウンに表示する。
+
+        # サーチ先指定（プルダウン）
+        # 検索結果の中から候補を選ぶ「絞り込み検索」
+        transit_goal = st.selectbox("サーチ先とするモンスターを選択してください（任意）", monsters_df)
+
+        col1, col2, col3 = st.columns(3)
+
+        with col1:
+            st.header("サーチ元")
+            st.write("マイクロ・コーダー")
+            st.write("マイクロ・コーダー")
+            st.write("マイクロ・コーダー")
+            st.markdown("---")
+            st.write("マイクロ・コーダー")
+            st.write("マイクロ・コーダー")
+            st.write("マイクロ・コーダー")
+
+        with col2:
+            st.header("経由")
+            st.write("斬機シグマ")
+            st.write("斬機サブトラ")
+            st.write("斬機アディオン")
+            st.markdown("---")
+            st.write("斬機シグマ")
+            st.write("斬機サブトラ")
+            st.write("斬機アディオン")
+
+        with col3:
+            st.header("サーチ先")
+            st.write("ガッチリ＠イグニスター")
+            st.write("ガッチリ＠イグニスター")
+            st.write("ガッチリ＠イグニスター")
+            st.markdown("---")
+            st.write("夢幻崩界イヴリース")
+            st.write("夢幻崩界イヴリース")
+            st.write("夢幻崩界イヴリース")
+
+    except AttributeError as ae:
+        st.error(ae)
+        st.error("""以下の点をご確認ください。
+                 (1)デッキレシピが「公開」になっているか
+                 (2)デッキに最低1体以上のモンスターが含まれているか""")
+    except Exception as e:
+        st.error("予期せぬ例外が発生しました。繰り返し発生する場合は以下のエラーメッセージを添えてお問い合わせフォームからご連絡ください。")
+        st.error(e)
