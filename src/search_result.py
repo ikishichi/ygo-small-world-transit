ddf06197f32cbc7734ce91f9bb78d5209994da25
shhig <shigihara5@gmail.com>
2023-08-20 22:45:30 +0900

    __get_monster_data_with_only_one_matching関数を実装

--- src/search_result.py
+++ src/search_result.py
@@ -1,41 +1,68 @@
 import pandas as pd
+import numpy as np
 class SearchResult:
-    def __init__(self, monster_list: pd.DataFrame, origin, destination=''):
+    def __init__(self, monsters_df: pd.DataFrame, origin, destination=''):
         """Initialize this class
         Args:
-            monster_list: (pandas) name, attribute, type, level, attack, defence
+            monsters_df: (pandas) name, attribute, type, level, attack, defence
             origin: for Search
             destination: for Search
         """
         self.search_result = [[]]
-        self.monster_list = monster_list
+        self.monsters_df = monsters_df
         self.origin = origin
         self.destination = destination
 
-    def __name_search(self, monster_list: pd.DataFrame, target_name):
+    def __get_monster_data_with_name(self, monsters_df: pd.DataFrame, target_name):
         """
             Return rows as pandas.Series type which contain strings matching target_name.
         Args:
-            monster_list: (pandas) name, attribute, type, level, attack, defence
+            monsters_df: (pandas) name, attribute, type, level, attack, defence
             target_name: for Search
         Return:
-            monster data with matching name
+            monster data with matching name (ndarray)
         """
-        return monster_list[monster_list['name'] == target_name]
+        return monsters_df[monsters_df['name'] == target_name].iloc[0]
+
+    def __get_monster_data_with_only_one_matching(self, monsters_df: pd.DataFrame, target_monster_data: np.ndarray):
+        """
+            Return rows as pandas.Series type which contain only one matching data with target_monster_data.
+        Args:
+            monsters_df: (pandas) name, attribute, type, level, attack, defence
+            target_monster_data: (pandas) name, attribute, type, level, attack, defence. for Search
+        Return:
+            monster list
+        """
+        # monsters_dfの一行ずつとtarget_monster_dataを突き合わせて、一つだけ一致するデータを取得し、リターン用のpd.DataFrameに格納する。
+        result = pd.DataFrame(columns=['name', 'attribute', 'type', 'level', 'attack', 'defence'])
+        #monsters_dfから1行ずつ比較
+        for index, row in monsters_df.iterrows():
+            match_count = 0
+            # 1要素だけ一致するデータを探す
+            for column_name in row.index:
+                if row[column_name] == target_monster_data[column_name]:
+                    match_count += 1
+                if match_count > 1:
+                    break
+            if match_count == 1:
+                result = pd.concat([result, row.to_frame().T])
+        return result
 
     def get(self):
         """Get search result
 
         Args:
-            monster_list: (pandas) name, attribute, type, level, attack, defence
+            monsters_df: (pandas) name, attribute, type, level, attack, defence
             origin: for Search
             destination: for Search
         Return:
-            2D list about destinations and transit points
-            [search_dest_A, [transit_1, transit_2, transit_3, ...]], 
-            [search_dest_B, [transit_1, transit_2, transit_3, ...]],
-            [search_dest_C, [transit_1, transit_2, transit_3, ...]],
-            ...
+            2D list about origin and transit points, destinations: pandas.dataframe
+            [
+                [search_origin_A, transit_A, search_dest_A], 
+                [search_origin_A, transit_B, search_dest_A], 
+                [search_origin_A, transit_C, search_dest_B], ...
+            ]
         """
-        name_search_result = self.__name_search(self.monster_list, self.origin)
-        return name_search_result
+        name_search_result = self.__get_monster_data_with_name(self.monsters_df, self.origin)
+        result = self.__get_monster_data_with_only_one_matching(self.monsters_df, name_search_result)
+        return result
--- test/search_result_test.py
+++ test/search_result_test.py
@@ -5,12 +5,12 @@ from src import search_result
 def test_get():
     print("\nStart!!!")
     test_pd = pd.DataFrame(columns=['name', 'attribute', 'type', 'level', 'attack', 'defence'])
-    test_pd = pd.DataFrame({'name':['テストマン','テストクリボー','LL-テスト・ダック'],
-                            'attribute':['光','闇','風'],
-                            'type':['サイバース族','悪魔族','鳥獣族'],
-                            'level':['10','1','1'],
-                            'attack':['100','0','50'],
-                            'defence':['300','200','0']})
+    test_pd = pd.DataFrame({'name':['テストマン','テストクリボー','LL-テスト・ダック','テストマンネオ'],
+                            'attribute':['光','闇','風','光'],
+                            'type':['サイバース族','悪魔族','鳥獣族','サイバース族'],
+                            'level':['10','1','1','10'],
+                            'attack':['100','0','50','10000'],
+                            'defence':['200','200','0','200']})
     print(test_pd)
     print()
     sr = search_result.SearchResult(test_pd,'LL-テスト・ダック')
