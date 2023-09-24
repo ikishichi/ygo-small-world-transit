1db1b76be822b9e03414ac6171ba2a72435f6a63
ikishichi <shino.aniesu1126@gmail.com>
2023-09-25 00:27:22 +0900

    monsters_dfをセッション内は保持する変数として持つように修正、検索ボタンを追加

--- src/ui.py
+++ src/ui.py
@@ -5,67 +5,69 @@ from deckinfo import DeckInfo
 from deck import Deck
 from search_result import SearchResult
 
+
 st.title("遊戯王スモール・ワールド乗り換え検索")
 st.caption("遊戯王DBの公開デッキを読み込むことで、<<スモール・ワールド>>のサーチ経路を検索できます。")
 
-columns = ['name', 'attribute', 'type', 'level', 'attack', 'defence']
-monsters_df = pd.DataFrame(columns=columns)
+# モンスターのDataFrameを保持するsession_state変数を定義
+if 'MONSTERS_DF' not in st.session_state:
+    columns = ['name', 'attribute', 'type', 'level', 'attack', 'defence']
+    st.session_state["MONSTERS_DF"] = pd.DataFrame(columns=columns)
 
-with st.form(key="deck_url"):
+try:
+    with st.form(key="deck_url"):
+        # URL入力欄の入力値
+        url = st.text_input("遊戯王DBの公開デッキのURLを入力してください。")
 
-    # URL入力欄の入力値
-    url = st.text_input("遊戯王DBの公開デッキのURLを入力してください。")
+        # デッキ取得ボタンの押下状態（boolean）
+        submit_btn = st.form_submit_button("デッキ取得")
 
-    # デッキ取得ボタンの押下状態（boolean）
-    submit_btn = st.form_submit_button("デッキ取得")
+    if submit_btn:
+        # デッキ情報（htmlバイナリデータ）を取得する
+        di = DeckInfo(url)
+        if di.is_success() is False:
+            st.warning("無効なURLです。遊戯王DBの公開デッキレシピのURLを入力してください。")
+            st.stop()
 
-    try:
-        if submit_btn:
-            # デッキ情報（htmlバイナリデータ）を取得する
-            di = DeckInfo(url)
-            if di.is_success() is False:
-                st.warning("無効なURLです。遊戯王DBの公開デッキレシピのURLを入力してください。")
-                st.stop()
+        # デッキからモンスターのDataFrameを取得する
+        deck = Deck(di.get())
+        st.session_state["MONSTERS_DF"] = deck.get_monsters_df()
 
-            # デッキからモンスターのDataFrameを取得する
-            deck = Deck(di.get())
-            monsters_df = deck.get_monsters_df()
-
+    with st.form(key='select_box'):
         # サーチ元指定（プルダウン。DataFrameの1列目が候補として表示される）
-        transit_start = st.selectbox("サーチ元となるモンスターを選択してください。", monsters_df)
-
-        # サーチ元に指定されたモンスターでSearchResultクラスに検索要求する
-        search_results = SearchResult(monsters_df, transit_start).get()
-
-        # TODO: (opt)検索結果のモンスター名一覧を取得し、サーチ先プルダウンに表示する。
+        transit_start = st.selectbox("サーチ元となるモンスターを選択してください。", st.session_state["MONSTERS_DF"])
 
         # サーチ先指定（プルダウン）
         # 検索結果の中から候補を選ぶ「絞り込み検索」
         # transit_goal = st.selectbox("サーチ先とするモンスターを選択してください（任意）", monsters_df)
 
-        col2, col3 = st.columns(2)
+        # 検索実行ボタン
+        search_btn = st.form_submit_button("検索")
 
-        # with col1:
-        #     st.header("サーチ元")
-        #     for origin in search_results["origin"]:
-        #         st.write(origin)
+    if search_btn:
+        # サーチ元に指定されたモンスターでSearchResultクラスに検索要求する
+        search_results = SearchResult(st.session_state["MONSTERS_DF"], transit_start).get()
 
-        with col2:
-            st.header("経由")
-            for transit in search_results["transit"]:
-                st.write(transit)
+        # TODO: (opt)検索結果のモンスター名一覧を取得し、サーチ先プルダウンに表示する。
 
-        with col3:
-            st.header("サーチ先")
-            for dest in search_results["dest"]:
-                st.write(dest)
+        if search_results is not None:
+            col2, col3 = st.columns(2)
+            with col2:
+                st.header("経由")
+                for transit in search_results["transit"]:
+                    st.write(transit)
 
-    except AttributeError as ae:
-        st.error(ae)
-        st.error("""以下の点をご確認ください。
-                 (1)デッキレシピが「公開」になっているか
-                 (2)デッキに最低1体以上のモンスターが含まれているか""")
-    except Exception as e:
-        st.error("予期せぬ例外が発生しました。繰り返し発生する場合は以下のエラーメッセージを添えてお問い合わせフォームからご連絡ください。")
-        st.error(e)
+            with col3:
+                st.header("サーチ先")
+                for dest in search_results["dest"]:
+                    st.write(dest)
+
+except AttributeError as ae:
+    st.error(ae)
+    st.error("""以下の点をご確認ください。
+             (1)デッキレシピが「公開」になっているか
+             (2)デッキに最低1体以上のモンスターが含まれているか""")
+except Exception as e:
+    st.error("予期せぬ例外が発生しました。繰り返し発生する場合は以下のエラーメッセージを添えてお問い合わせフォームからご連絡ください。")
+    st.error(e)
 
